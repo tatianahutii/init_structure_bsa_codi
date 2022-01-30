33090a47a8f9f767041473d4de7d5e7af1dfc853
Tati Hutii <tatiana.gutiy@gmail.com>
2022-01-30 16:11:33 +0200

    Add init projects
    Delete backend/Core/Codi.Core.WebAPI/.vs/Codi.Core.WebAPI directory
    
    Delete vs folder
    
    Delete backend/Core/Codi.Core.WebAPI/Codi.Core.WebAPI/bin/Debug/net6.0 directory
    
    delete bin
    
    Delete backend/Core/Codi.Core.WebAPI/Codi.Core.WebAPI/obj directory
    
    delete obj
    
    Delete backend/Core/Codi.Core.WebAPI/Codi. Core.DAL/obj directory
    
    delete obj
    
    Delete backend/Core/Codi.Core.WebAPI/Codi.Core.BL/obj directory
    
    delete obj
    
    Delete backend/Core/Codi.Core.WebAPI/Codi.Core.DAL/obj directory
    
    delete obj
    
    rename file
    
    Add rest projects

---
+++ backend/Core/Codi.Core/Codi. Core.DAL/Context/CodiCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Codi.Core.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Codi._Core.DAL
+{
+    public class CodiCoreContext : DbContext
+    {
+        public DbSet<Sample> Samples { get; private set; }
+        public CodiCoreContext(DbContextOptions<CodiCoreContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Configure();
+            //modelBuilder.Seed();
+        }
+    }
+}
---
+++ backend/Core/Codi.Core/Codi.Core.BL/MappingProfiles/SampleProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Codi.Core.Common.DTO;
+using Codi.Core.DAL.Entities;
+
+namespace Codi.Core.BL.MappingProfiles
+{
+    public sealed class SampleProfile : Profile
+    {
+        public SampleProfile()
+        {
+            CreateMap<Sample, SampleDto>();
+
+            CreateMap<SampleDto, Sample>();
+            CreateMap<NewSampleDto, Sample>();
+        }
+    }
+}
